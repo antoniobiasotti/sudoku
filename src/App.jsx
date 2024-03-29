bfb518a86947f8ffd4cbd851f0468265b260b71d
Antonio <abamorim36@gmail.com>
2024-03-29 18:18:00 -0300

    Game component - sudoku hbars

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './Header.jsx'
 import { Header } from './Header.jsx'
+import { Game } from './Game.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Game/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/Game.jsx
@@ -0,0 +1,15 @@
+import styles from './Game.module.css';
+
+export function Game() {
+     return (
+          <div>
+               <hr className={styles.hbar}></hr>
+               <hr className={styles.hbar}></hr>
+               <hr className={styles.hbar}></hr>
+          </div>
+               // <div className={styles.vbar}>
+
+               // </div>
+          
+     )
+}
