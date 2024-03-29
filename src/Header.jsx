40549cc6aac6fc6109926e1eb25c6b15e38e9a59
Antonio <abamorim36@gmail.com>
2024-03-29 18:08:50 -0300

    Component Header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+export function Header() {
+     return (
+          <header className={styles.header}>
+               <h2>Bem-vindo ao Sudoku Master!!</h2>
+          </header>
+     )
+}
